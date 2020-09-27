5c3dfcb22952868c5c59c61f872f64c8cbaec44a
Pavan <pavantejakusunuri@gmail.com>
2020-09-27 12:50:52 +0530

    Added Card and Cardlist components

--- src/components/card-list/cardlist.jsx
+++ src/components/card-list/cardlist.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+import { Card } from "../card/card";
 import "./cardlist.css";
 
 export const CardList = (props) => {
   return (
     <div className="card-list">
       {props.monsters.map((monster) => (
-        <h1 key={monster.id}>{monster.name}</h1>
+        <Card key={monster.id} monster={monster} />
       ))}
     </div>
   );
